b6f9d9a42b48f96e8ff6e55d170e2709f8258da9
Manouel <manuel.cha34@gmail.com>
2015-11-03 15:17:48 +0100

    Affichage des posts sur la page d'accueil

--- blog-pratik/src/BlogBundle/Controller/BlogController.php
+++ blog-pratik/src/BlogBundle/Controller/BlogController.php
@@ -13,13 +13,21 @@ class BlogController extends Controller
         $post = $em->getRepository('BlogBundle:Post')->find($postId);
         return $post;
     }
+    
+    private function getPosts()
+    {
+        $em = $this->getDoctrine()->getManager();
+        return $em->getRepository('BlogBundle:Post')->findAll();
+    }
 
     /**
      * @Route("/", name="blogpratik_homepage")
      */
     public function indexAction()
     {
-        return $this->render('BlogBundle:Blog:index.html.twig');
+        
+        return $this->render('BlogBundle:Blog:index.html.twig',
+                                            array('posts' => $this->getPosts()));
     }
 
     /**
