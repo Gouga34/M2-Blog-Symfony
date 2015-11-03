9820eec9902f24066f2adb1ebc46a35b55b5aa2a
Geoffrey DUMAS <geoffreydms@gmail.com>
2015-11-03 16:48:14 +0100

    Ajout de bootstrap css

--- blog-pratik/app/AppKernel.php
+++ blog-pratik/app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new BlogBundle\BlogBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
