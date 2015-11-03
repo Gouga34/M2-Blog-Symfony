f4ffedf4112a604cf629942915dba81bc7345535
Gouga34 <imala34@hotmail.fr>
2015-11-03 14:10:52 +0100

    Creation entity Post

--- Bd/bd.sql
+++ Bd/bd.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.1.1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Jeu 29 Octobre 2015 à 14:07
--- Version du serveur :  5.5.42
--- Version de PHP :  5.6.7
+-- Client: localhost
+-- Généré le: Mar 03 Novembre 2015 à 14:09
+-- Version du serveur: 5.5.46-0ubuntu0.14.04.2
+-- Version de PHP: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Base de données :  `partikBd`
+-- Base de données: `blog-pratik`
 --
 
 -- --------------------------------------------------------
@@ -20,19 +26,15 @@ SET time_zone = "+00:00";
 -- Structure de la table `post`
 --
 
-CREATE TABLE `post` (
-  `title` varchar(50) NOT NULL,
-  `url_alias` varchar(100) NOT NULL,
-  `content` text NOT NULL,
-  `pubilshed` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `url_alias` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `published` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `post`
---
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`title`);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
