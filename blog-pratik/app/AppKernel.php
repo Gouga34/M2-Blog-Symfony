f67f732f3fadaef11fd4025836a04ca710d4e1cc
Manouel <manuel.cha34@gmail.com>
2015-11-11 11:06:05 +0100

    Mise à jour du AppKernel pour FOSUserBundle

--- blog-pratik/app/AppKernel.php
+++ blog-pratik/app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new BlogBundle\BlogBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
